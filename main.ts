7f78519dbfe4a2b8bf8c515319c3aa40ce5253df
970117 <91246944+970117@users.noreply.github.com>
2021-09-23 16:05:03 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,57 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    for (let Y = 0; Y <= 4; Y++) {
+        for (let X = 0; X <= 4; X++) {
+            led.plot(X, Y)
+            basic.pause(200)
+        }
+    }
+    basic.showLeds(`
+        # # # # #
+        # . . . #
+        # . . . #
+        . # . # .
+        . . # . .
+        `)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    for (let Y = 0; Y <= 4; Y++) {
+        for (let X = 0; X <= 4; X++) {
+            led.plot(X, Y)
+            basic.pause(200)
+        }
+    }
+    basic.showLeds(`
+        # # # # #
+        # . . . #
+        # . # . #
+        # . . . #
+        # # # # #
+        `)
+})
 for (let Y = 0; Y <= 4; Y++) {
     for (let X = 0; X <= 4; X++) {
         led.plot(X, Y)
         basic.pause(200)
     }
 }
-basic.forever(function () {
-	
-})
+basic.showLeds(`
+    . . # . .
+    . . # . .
+    # # # # #
+    . . # . .
+    . . # . .
+    `)
